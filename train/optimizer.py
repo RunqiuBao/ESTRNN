98ff24dd2fe0d0210a6a3f830781c17ea3a602f1
RunqiuBao <35663368+RunqiuBao@users.noreply.github.com>
2021-04-23 16:18:51 +0900

    Update optimizer.py

--- train/optimizer.py
+++ train/optimizer.py
@@ -2,6 +2,12 @@ from importlib import import_module
 
 import torch.optim.lr_scheduler as lr_scheduler
 
+def lr_func(epoch, max_lr=1, min_lr=0, warmup_epoch=100, end_epoch=500):
+    if epoch<100:
+        y = max_lr
+    else:
+        y = (5/4-epoch/400)*max_lr
+    return y
 
 class Optimizer:
     def __init__(self, para, target):
@@ -23,6 +29,8 @@ class Optimizer:
             elif para.lr_scheduler == "cosineW":
                 self.scheduler = lr_scheduler.CosineAnnealingWarmRestarts(self.optimizer, T_0=10, T_mult=2,
                                                                           eta_min=1e-8)
+            elif para.lr_scheduler == "lr_bit":
+                self.scheduler = lr_scheduler.LambdaLR(optimizer=self.optimizer, lr_lambda=lr_func)
             else:
                 raise NotImplementedError
         except:
